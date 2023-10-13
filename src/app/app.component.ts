770a8fde097042ff426e603986dbd2987282896d
KonnosCh <elenischris@gmail.com>
2023-10-13 17:21:19 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,6 @@ import { CommonModule } from '@angular/common';
 })
 export class AppComponent {
   title = 'angular-introduction';
-  name: string = "Christantis";
+  name: string = "Xristantis";
   lastname = "Elenis";
 }
